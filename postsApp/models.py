a9d25caaf8fb7e9cab76c102144c8b18d95b43c6
shiv-raj-15 <shivaniswaraj68702@gmail.com>
2020-11-24 02:26:22 +0530

    resolves: #19

--- postsApp/models.py
+++ postsApp/models.py
@@ -6,12 +6,10 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    post_image = models.ImageField(
-        upload_to='postsApp/posts', blank=True, null=True)
+    post_image = models.ImageField(upload_to='postsApp/posts', blank=True, null=True)
     date_posted = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    video = models.FileField(upload_to='postsApp/videos',
-                             default=None, blank=True, null=True)
+    video = models.FileField(upload_to='postsApp/videos', default=None, blank=True, null=True)
 
     def __str__(self):
         return self.title
