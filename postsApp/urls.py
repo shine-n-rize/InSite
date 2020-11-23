7b24d74f81c6017f95b6b1b8302e43077f31c8a5
shiv-raj-15 <shivaniswaraj68702@gmail.com>
2020-11-23 19:28:32 +0530

    resolves: #11, #12

--- postsApp/urls.py
+++ postsApp/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path, include
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 urlpatterns = [
-    path('add-new/', views.new_post, name='new'),
-    path('gallery/', views.gallery, name='gallery'),
+    path('gallery/', PostListView.as_view(), name='gallery'),
+    path('add/', PostCreateView.as_view(), name='post-add'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
 ]
--- postsApp/views.py
+++ postsApp/views.py
@@ -1,32 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
+from django.contrib.auth.models import User
 
-posts = [
-    {
-        'user': 'v_shh',
-        'title': 'First',
-        'description': 'My first post',
-        'date_posted': '12 Nov, 2020',
-    },
-    {
-        'user': 'D',
-        'title': 'Second',
-        'description': 'My second post',
-        'date_posted': '14 Nov, 2020',
-    },
-    {
-        'user': 'mia',
-        'title': 'Third',
-        'description': 'My third post',
-        'date_posted': '16 Nov, 2020',
-    },
-]
+
 
 def new_post(request):
     return render(request, 'postsApp/new_post.html')
+
 
 def gallery(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'postsApp/gallery.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'postsApp/gallery.html'     # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'postsApp/post_form.html'     # <app>/<model>_<viewtype>.html
+    fields = ['post_image', 'title', 'description']
+    success_url = '../gallery'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'description']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.user:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '../gallery'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.user:
+            return True
+        return False
